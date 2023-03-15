32a2a6dab2510312e6d35c96d42a42a33679aed3
Vladimir <vovakovalev2016@icloud.com>
2023-03-15 19:03:10 +0300

    complete project

---
+++ TodoeyCourseProject/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Todoey
+//
+//  Created by Vladimir Kovalev on 15.03.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+let CATEGORY_CELL = "CategoryCell"
+let GO_TO_ITEMS = "goToItems"
+let TO_DO_ITEM_CELL = "ToDoItemCell"
